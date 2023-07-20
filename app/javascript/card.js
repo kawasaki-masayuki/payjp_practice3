2a81428974c702ad9470a1b5cedad061ae72c5af
kawasaki-masayuki <purple-wisteria@outlook.jp>
2023-07-20 12:04:47 +0900

    fourth commit

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_5de2005d7f32a6da049e2f7b')// PAY.JPテスト公開鍵
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
